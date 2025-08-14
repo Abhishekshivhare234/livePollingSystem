81cf684dd4c2585da995def6809d876c8edd7289
Abhishekshivhare234 <abhishivhare8962@gmail.com>
2025-08-15 03:17:55 +0530

    change origin

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3000,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5173'
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://live-polling-system-seven-gamma.vercel.app'
 };
 
 module.exports = config;
